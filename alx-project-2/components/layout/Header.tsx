4bc612ff425e0eb61948475819c41f16e9816693
kemiashaju <kemiolugbogi0@gmail.com>
2025-11-18 14:21:15 +0100

    Header

---
+++ alx-project-2/components/layout/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="w-full p-4 bg-blue-600 text-white text-center">
+      <h1 className="text-2xl font-bold">ALX Next.js Project</h1>
+    </header>
+  );
+};
+
+export default Header;
