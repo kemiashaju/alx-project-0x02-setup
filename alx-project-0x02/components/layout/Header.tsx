6122d74c404b98b4327847c890360670a5a479e9
kemiashaju <kemiolugbogi0@gmail.com>
2025-11-19 15:06:35 +0100

    Header

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
-    <header className="p-4 bg-blue-600 text-white text-center">
-      <h1 className="text-2xl font-bold">Welcome to ALX Project 2</h1>
+    <header className="p-4 bg-blue-600 text-white flex justify-center gap-6">
+      <Link href="/home" className="hover:underline">Home</Link>
+      <Link href="/about" className="hover:underline">About</Link>
     </header>
   );
 }
+
