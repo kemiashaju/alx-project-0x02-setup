d0e731bdcad0c049dfa21c13c46cbbac6977e5c1
kemiashaju <kemiolugbogi0@gmail.com>
2025-11-19 15:00:58 +0100

    home

---
+++ alx-project-0x02/pages/home.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold">Home Page</h1>
+      <p>Welcome to the Home Page!</p>
+    </main>
+  );
+}
