1f79540ad88567a29c71b5885e0f7d600268468c
kemiashaju <kemiolugbogi0@gmail.com>
2025-11-19 15:03:41 +0100

    about

---
+++ alx-project-0x02/pages/about.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold">About Page</h1>
+      <p>This is the About Page.</p>
+    </main>
+  );
+}
