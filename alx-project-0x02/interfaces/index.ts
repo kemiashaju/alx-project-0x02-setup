8833f5237ca03eaf59aafb154517cae34b7fa9c4
kemiashaju <kemiolugbogi0@gmail.com>
2025-11-20 13:48:19 +0100

    index

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -7,3 +7,13 @@ export interface CardProps {
   title: string;
   content: string;
 }
+
+// interfaces/index.ts
+
+export interface ButtonProps {
+  size?: "small" | "medium" | "large";
+  shape?: "rounded-sm" | "rounded-md" | "rounded-full";
+  children: React.ReactNode;
+  onClick?: () => void;
+}
+
