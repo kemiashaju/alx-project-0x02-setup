8943518207e92a20e5df960f7e4b919d89ac4cc5
kemiashaju <kemiolugbogi0@gmail.com>
2025-11-20 13:50:14 +0100

    about

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,8 +1,15 @@
-export default function AboutPage() {
+// pages/about.tsx
+import Button from "../components/common/Button";
+
+export default function About() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold">About Page</h1>
-      <p>This is the About Page.</p>
-    </main>
+    <div className="flex flex-col items-center justify-center min-h-screen gap-6 p-4">
+      <h1 className="text-3xl font-bold">About Page</h1>
+      <div className="flex flex-wrap gap-4 mt-4">
+        <Button size="small" shape="rounded-sm">Small Sm</Button>
+        <Button size="medium" shape="rounded-md">Medium Md</Button>
+        <Button size="large" shape="rounded-full">Large Full</Button>
+      </div>
+    </div>
   );
 }
