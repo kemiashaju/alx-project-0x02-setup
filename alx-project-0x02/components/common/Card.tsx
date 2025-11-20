cc6c233c99473eb5cc283fe3dbb2e31f49c89115
kemiashaju <kemiolugbogi0@gmail.com>
2025-11-20 12:26:58 +0100

    Card

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { CardProps } from "../../interfaces";
 
-
-const Card: React.FC<CardProps> = ({ title, content }) => {
+const Card = ({ title, content }: CardProps) => {
   return (
     <div className="border rounded-lg p-4 shadow-md bg-white w-[250px]">
       <h2 className="text-xl font-semibold mb-2">{title}</h2>
