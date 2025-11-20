ed5c728894296a70c95344902c82a1bfd95f9257
kemiashaju <kemiolugbogi0@gmail.com>
2025-11-20 14:21:56 +0100

    Header

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,11 +1,23 @@
+// components/layout/Header.tsx
 import Link from "next/link";
 
-export default function Header() {
+const Header = () => {
   return (
-    <header className="p-4 bg-blue-600 text-white flex justify-center gap-6">
-      <Link href="/home" className="hover:underline">Home</Link>
-      <Link href="/about" className="hover:underline">About</Link>
+    <header className="w-full bg-blue-600 text-white p-4 shadow-md">
+      <nav className="flex justify-center gap-6">
+        <Link href="/home" className="hover:underline">
+          Home
+        </Link>
+        <Link href="/about" className="hover:underline">
+          About
+        </Link>
+        <Link href="/posts" className="hover:underline">
+          Posts
+        </Link>
+      </nav>
     </header>
   );
-}
+};
+
+export default Header;
 
