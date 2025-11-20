1ee23fea0b3ef98e4558dde0bf2a8a27d8c09fe2
kemiashaju <kemiolugbogi0@gmail.com>
2025-11-20 11:09:34 +0100

    Card

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,10 +1,14 @@
-import { CardProps } from "@/interfaces";
+import React from "react";
+import { CardProps } from "../../interfaces";
 
-export default function Card({ title, content }: CardProps) {
+
+const Card: React.FC<CardProps> = ({ title, content }) => {
   return (
-    <div className="border rounded-lg p-6 shadow-md hover:shadow-lg transition-shadow w-full max-w-sm bg-white">
-      <h2 className="text-xl font-bold mb-2">{title}</h2>
+    <div className="border rounded-lg p-4 shadow-md bg-white w-[250px]">
+      <h2 className="text-xl font-semibold mb-2">{title}</h2>
       <p className="text-gray-700">{content}</p>
     </div>
   );
-}
+};
+
+export default Card;
