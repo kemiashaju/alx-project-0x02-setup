46537d560e8e598c8820bcce7ed5c3993eeb9b4c
kemiashaju <kemiolugbogi0@gmail.com>
2025-11-20 14:27:51 +0100

    about

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,15 +1,25 @@
-// pages/about.tsx
+import Header from "../components/layout/Header";
 import Button from "../components/common/Button";
 
 export default function About() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen gap-6 p-4">
-      <h1 className="text-3xl font-bold">About Page</h1>
-      <div className="flex flex-wrap gap-4 mt-4">
-        <Button size="small" shape="rounded-sm">Small Sm</Button>
-        <Button size="medium" shape="rounded-md">Medium Md</Button>
-        <Button size="large" shape="rounded-full">Large Full</Button>
+    <>
+      <Header />
+      <div className="flex flex-col items-center justify-center min-h-screen gap-6 p-4">
+        <h1 className="text-3xl font-bold">About Page</h1>
+        <div className="flex flex-wrap gap-4 mt-4">
+          <Button size="small" shape="rounded-sm">
+            Small Sm
+          </Button>
+          <Button size="medium" shape="rounded-md">
+            Medium Md
+          </Button>
+          <Button size="large" shape="rounded-full">
+            Large Full
+          </Button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
+
