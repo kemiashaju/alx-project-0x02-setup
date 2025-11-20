09c3faf81209358263a7b2fca138948e6f39189c
kemiashaju <kemiolugbogi0@gmail.com>
2025-11-20 14:29:31 +0100

    posts

---
+++ alx-project-0x02/pages/posts.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/layout/Header";
+
+export default function Posts() {
+  return (
+    <>
+      <Header />
+      <div className="flex flex-col items-center justify-center min-h-screen gap-6 p-4">
+        <h1 className="text-3xl font-bold">Posts Page</h1>
+        <p className="text-gray-700">This page will display posts.</p>
+      </div>
+    </>
+  );
+}
